cdd7d53612f32a784b12335c42dac81adca5f381
DomYidi <programacionyidi@gmail.com>
2022-08-18 14:40:33 -0500

    Falta agregar la musica

---
+++ Assets/AudioPlay.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audioClip;
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void PlayAudio(int place, float volume)
+    {
+        audioSource.PlayOneShot(audioClip[place], volume);
+    }
+}
